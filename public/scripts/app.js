ee217d072288c593a864e63ea5065a5ddc5adcf5
fatuma ismail <timaismail90@gmail.com>
2019-01-25 20:56:49 +0000

    app.js updated to include the path to the styled date coming from the database. All working good!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,105 +1,105 @@
-$(document).ready(function() {
-    $(".max-length").hide();
-    /*
-     * Client-side JS logic goes here
-     * jQuery ialready loaded
-     * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-     */
+// $(document).ready(function() {
+// $(".max-length").hide();
+/*
+ * Client-side JS logic goes here
+ * jQuery ialready loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
 
 
-    // Test / driver code (temporary). Eventually will get this from the server.
+// Test / driver code (temporary). Eventually will get this from the server.
 
-    const data = [{
-            "user": {
-                "name": "Newton",
-                "avatars": {
-                    "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                    "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                    "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-                },
-                "handle": "@SirIsaac"
+const data = [{
+        "user": {
+            "name": "Newton",
+            "avatars": {
+                "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+                "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+                "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
             },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
+            "handle": "@SirIsaac"
         },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": {
-                    "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                    "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-                    "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-                },
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
+        "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
         },
-        {
-            "user": {
-                "name": "Johann von Goethe",
-                "avatars": {
-                    "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                    "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                    "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-                },
-                "handle": "@johann49"
+        "created_at": 1461116232227
+    },
+    {
+        "user": {
+            "name": "Descartes",
+            "avatars": {
+                "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+                "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+                "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
             },
-            "content": {
-                "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+            "handle": "@rd"
+        },
+        "content": {
+            "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
+    },
+    {
+        "user": {
+            "name": "Johann von Goethe",
+            "avatars": {
+                "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+                "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+                "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
             },
-            "created_at": 1461113796368
-        }
-    ];
+            "handle": "@johann49"
+        },
+        "content": {
+            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+        },
+        "created_at": 1461113796368
+    }
+];
 
 
 
 
-    function createTweetElement(tweet) {
+function createTweetElement(tweet) {
 
-        const renderedTweet =
-            `<article class="article-tweet">
+    const renderedTweet =
+        `<article class="article-tweet">
           <header>
 
-            <div class= "img">
+            <div class= "avatar" ${tweet.user.avatars.small}>
               <img src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png" alt="av" />
             </div>
 
-            <h2>Bill Fields</h2>
-            <span> @Mr.Fields </span>
+            <span class= "username">${tweet.user.name}</span>
+            <span class="handle"> ${tweet.user.handle}</span>
           </header>
           <p>${tweet.content.text}</p>
               <hr>
-          <footer class= "b">
+          <footer class= "b">${tweet.created_at}
                 10 days ago
           </footer>
         </article>`
 
-        return renderedTweet
+    return renderedTweet
 
-    }
-
-
-    function renderTweets(tweets) {
-        tweets.forEach(function(tweet) {
-            // Create a new p tag
-            var newTweetElement = createTweetElement(tweet)
-
-            $('#tweet-container').prepend(newTweetElement)
-        });
+}
 
 
-    }
+function renderTweets(tweets) {
+    tweets.forEach(function(tweet) {
+        // Create a new p tag
+        var newTweetElement = createTweetElement(tweet)
 
-    // renderTweets(data);
+        $('#tweet-container').prepend(newTweetElement)
+    });
 
-    // form submission assignment found below
 
+}
 
+// renderTweets(data);
+
+// form submission assignment found below
+
+function newTweet() {
     $("form").submit(function(event) {
         event.preventDefault();
         $(".max-length").hide();
@@ -127,35 +127,39 @@ $(document).ready(function() {
         }
 
     });
+}
 
-    // AJAX GET Request
+// AJAX GET Request
 
-    function loadTweets() {
-        $.ajax({
-            url: '/tweets',
-            type: 'GET',
-            dataType: 'json',
-            success: function(tweets) {
-                console.log(tweets)
-                renderTweets(tweets);
-            }
-        })
-    }
-
-
-    // function for the toggle button below:
-    $('#nav-bar button').click(function() {
-        $('.new-tweet').slideToggle(slow)
+function loadTweets() {
+    $.ajax({
+        url: '/tweets',
+        type: 'GET',
+        dataType: 'json',
+        success: function(tweets) {
+            console.log(tweets)
+            renderTweets(tweets);
+        }
+    })
+}
 
 
-        $('.new-tweet textarea').focus();
+// function for the toggle button below:
+$('#nav-bar button').click(function() {
+    $('.new-tweet').slideToggle(slow)
 
 
-    });
+    $('.new-tweet textarea').focus();
+
+
+});
 
 
 
-
+$(document).ready(function() {
+    $(".max-length").hide();
+    loadTweets();
+    newTweet();
 
 
 });
